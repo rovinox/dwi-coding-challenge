70943245546ab82e4fb309c5a0ba503b3a7844aa
neaj mahmud <neajmahmud230@gmail.com>
2019-07-31 11:57:32 -0400

    done

--- src/components/ZipCode.js
+++ src/components/ZipCode.js
@@ -23,6 +23,8 @@ export default class ZipCode extends Component {
             cancelToken: this.cancel.token
 		} ).then(res =>{
             this.setState({result:[res.data]})
+        }).catch( err =>{
+            console.log(err);
         })
         
     }
@@ -40,17 +42,18 @@ export default class ZipCode extends Component {
         const {result} = this.state
         const displayinfo = result.map( zip =>{
             return <div>
-                    <Container style={{marginTop:"10px", fontSize:"30px"}}>
-                        {zip.response.city}, {zip.response.stateCode} {zip.response.zipCode}
-                    </Container>
+                    
                     <Container>
-                    {zip.response.stateCode === "CT" ? <h1>Hello i see you're from connecticut</h1> : null}
+                        {zip.response.stateCode === "CT" ? <h1>Hello, i see you're from {zip.response.city}, {zip.response.stateCode}.</h1> : <Container style={{marginTop:"10px", fontSize:"30px"}}>
+                        {zip.response.city}, {zip.response.stateCode} {zip.response.zipCode}
+                        </Container>}
                     </Container>
 
                    </div>
         })
         return (
             <div className="zip-component">
+                    <h2>Please insert your zip code</h2>
                 <Container>
                     <input  style={{marginTop:"30px", fontSize:"30px"}}onChange={this.handleInput}></input>
                 </Container>     
